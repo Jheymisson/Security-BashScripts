5e84b8b1e58e9a11f7f4f0ad9a8bc4d2918987a0
Jheymisson <jheymis.albuquerque@hotmail.com>
2022-06-27 19:08:35 -0300

    ajuste no arquivo de port scanner

--- portscan.sh
+++ portscan.sh
@@ -4,7 +4,7 @@ if [ "$1" == "" ]
 then
 	echo "PORTSCAN NETWORK"
 	echo "Modo de uso: $0 REDE PORTA"
-	echo "Exemplo: $0 199.0.0.0 8080"
+	echo "Exemplo: $0 199.0.0.0 80"
 else
 for ip in {1..254};
 do
